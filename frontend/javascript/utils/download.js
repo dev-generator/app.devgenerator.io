07d2be3b78eabddd7662949dc673354bd41159d5
Christopher Pezza <chris.pezza@hey.com>
2020-11-29 00:50:46 -0500

    add download btn class

---
+++ frontend/javascript/utils/download.js
@@ -0,0 +1,18 @@
+import {CONSTANTS} from '../constants';
+
+export default class Download {
+  constructor(code, filename, filemeta) {
+    this.button = document.getElementById('download-btn');
+
+    if (window.location.pathname.includes(CONSTANTS.GENERATORS)) {
+      this.button.onclick = () => {
+        var filename = filename;
+        var blob = new Blob([code], {
+          type: filemeta,
+        });
+
+        saveAs(blob, filename);
+      };
+    }
+  }
+}
