375d1fb9244d59111b9b9fd05bbf168c5a084314
Christopher Pezza <chris.pezza@hey.com>
2020-11-28 23:15:35 -0500

    add constants file

---
+++ frontend/javascript/constants.js
@@ -0,0 +1,86 @@
+const CONSTANTS = {
+  BLANKSPACE: ' ',
+  BLANK: '',
+  DARK: 'dark',
+  LIGHT: 'light',
+  THEME: 'theme',
+  PREFERSCHEME: '(prefers-color-scheme: dark)',
+  TRUE: 'true',
+  FALSE: 'false',
+  CLASS: 'class',
+  HIDDEN: 'hidden',
+  VISIBLECLASS: 'visibleClass',
+  INVISIBLECLASS: 'invisibleClass',
+  ACTIVECLASS: 'activeClass',
+  ENTERINGCLASS: 'enteringClass',
+  ENTERTIMEOUT: 'enterTimeout',
+  LEAVINGCLASS: 'leavingClass',
+  LEAVINGTIMEOUT: 'leaveTimeout',
+  DARKMODE: 'darkMode',
+  ON: 'on',
+  ACTIVETARGET: 'activeTarget',
+  TARGETS: {
+    MENU: 'menu',
+    OVERLAY: 'overlay',
+    SIDEBAR: 'sidebar',
+    TOGGLE: 'toggle',
+    ONICON: 'onIcon',
+    OFFICON: 'offIcon',
+  },
+  DOM: {
+    HTML: 'html',
+    SVG: 'svg',
+  },
+  TEXT: {
+    WHITE: 'text-white',
+    GRAY800: 'text-gray-800',
+    DARK: {
+      GRAY100: 'dark:text-gray-100',
+    },
+    HOVER: {
+      WHITE: 'hover:text-white',
+    },
+    GROUPHOVER: {
+      GRAY50: 'group-hover:text-gray-50',
+    },
+  },
+  BG: {
+    PURPLE700: 'bg-purple-700',
+    GRAY200: 'bg-gray-200',
+    HOVER: {
+      PURPLE500: 'hover:bg-purple-500',
+    },
+  },
+  OPACITY: {
+    O100: 'opacity-100',
+    O0: 'opacity-0',
+  },
+  TRANSLATE: {
+    X0: 'translate-x-0',
+    X7: 'translate-x-7',
+    XFULL: 'translate-x-full',
+    Y0: 'translate-y-0',
+    Y4: 'translate-y-4',
+    NEGXFULL: '-translate-x-full',
+    SM: {
+      Y0: 'translate-y-0',
+    },
+  },
+  EASE: {
+    IN: 'ease-in',
+    OUT: 'ease-out',
+  },
+  DURATION: {
+    D100: 'duration-100',
+    D200: 'duration-200',
+    D300: 'duration-300',
+  },
+  SCALE: {
+    SM: {
+      S100: 'sm:scale-100',
+      S95: 'sm:scale-95',
+    },
+  },
+};
+
+export {CONSTANTS};
