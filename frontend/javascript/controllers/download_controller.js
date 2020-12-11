c9907c882d2381ac3380eb301f5445dcae5a1f85
Christopher Pezza <chris.pezza@hey.com>
2020-12-11 17:35:16 -0500

    upgrade download button to stimulus

---
+++ frontend/javascript/controllers/download_controller.js
@@ -0,0 +1,16 @@
+import {Controller} from 'stimulus';
+import {CONSTANTS} from '../constants';
+import {saveAs} from 'file-saver';
+
+export default class extends Controller {
+  static targets = [CONSTANTS.TARGETS.OUTPUT];
+  static values = { filename: String, filemeta: String };
+
+  download() {
+    var blob = new Blob([this.outputTarget.textContent.trim(1)], {
+      type: this.filemetaValue,
+    });
+
+    saveAs(blob, this.filenameValue);
+  }
+}
