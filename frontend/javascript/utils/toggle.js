1f3cbac942d2351b7aebc8c784b33a8ced24ecd1
Christopher Pezza <chris.pezza@hey.com>
2020-12-12 21:25:40 -0500

    remove size param

--- frontend/javascript/utils/toggle.js
+++ frontend/javascript/utils/toggle.js
@@ -1,18 +1,17 @@
 import {CONSTANTS} from '../constants';
 
 export default class Toggle {
-  constructor(id, size) {
+  constructor(id) {
     this.button = document.getElementById(id);
     this.toggle = this.button.children[1];
     this.onIcon = this.button.children[1].children[1];
     this.offIcon = this.button.children[1].children[0];
-    this.size = size || CONSTANTS.TRANSLATE.X5;
   }
 
   turnOn() {
     this.button.classList.add(CONSTANTS.BG.PURPLE700);
     this.toggle.classList.remove(CONSTANTS.TRANSLATE.X0);
-    this.toggle.classList.add(this.size);
+    this.toggle.classList.add(CONSTANTS.TRANSLATE.X5);
     this.offIcon.classList.remove(CONSTANTS.OPACITY.O100);
     this.offIcon.classList.add(CONSTANTS.OPACITY.O0);
     this.onIcon.classList.remove(CONSTANTS.OPACITY.O0);
@@ -22,7 +21,7 @@ export default class Toggle {
 
   turnOff() {
     this.button.classList.add(CONSTANTS.BG.GRAY200);
-    this.toggle.classList.remove(this.size);
+    this.toggle.classList.remove(CONSTANTS.TRANSLATE.X5);
     this.toggle.classList.add(CONSTANTS.TRANSLATE.X0);
     this.offIcon.classList.remove(CONSTANTS.OPACITY.O0);
     this.offIcon.classList.add(CONSTANTS.OPACITY.O100);
