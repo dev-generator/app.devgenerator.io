284a78187dc10088c1881d329802cb9167f04189
Christopher Pezza <chris.pezza@hey.com>
2020-12-12 21:27:53 -0500

    migrate file upload to stimulus controller

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,10 +1,7 @@
-import 'bridgetown-quick-search';
 import {Application} from 'stimulus';
 import {definitionsFromContext} from 'stimulus/webpack-helpers';
 
 import '../styles/index.scss';
-import './utils/file-upload';
-import './generators/node_modules/packagejson';
 
 const application = Application.start();
 const context = require.context('./controllers', true, /\.js$/);
